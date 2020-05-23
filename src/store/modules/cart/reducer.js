bde09b2599a4310bf37ff2abe2a58568c29e722a
Marciel De Liz Santos <marcieldelizsantos@MacBook-Air-de-Marciel.local>
2020-05-23 11:02:49 -0300

    alterando quantidades no carrinho

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -29,6 +29,20 @@ export default function cart(state = [], action){
         }
       });
 
+    case '@cart/UPDATE_AMOUNT': {
+      if(action.amount <= 0){
+        return state;
+      }
+
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id); //id está vindo direto da action
+
+        if(productIndex >= 0){
+           draft[productIndex].amount = Number(action.amount);
+        }
+      });
+    }
+
     default:
        return state;
   }
